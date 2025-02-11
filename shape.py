1c4c6ea5f32f7d0817af3a47dc63f6b577c954ca
Asher Harvey-Smith <asherharveysmith@gmail.com>
2025-02-11 23:49:20 +0000

    comments babyy

--- shape.py
+++ shape.py
@@ -4,46 +4,63 @@ from math import floor
 import matplotlib.pyplot as plt
 
 def split(image):
-    # this will be disgusting to read and I can only apologise
-    data = image[:, :, 0] != 255
+    data = image[:, :, 0] != 255 # bitarray with ones where nonwhite
     h, w = data.shape
-    p = np.zeros((h, w, 2), dtype = np.dtype(int))
+
+    # we use a simple disjoint set data structure represented by a parent matrix
+    # see https://en.wikipedia.org/wiki/Disjoint-set_data_structure
+    # union just prioritises the left parent over the top one, no union by rank/size business
+    # also no path splitting/merging
+    # parent matrix with shape: height × width × 2 (parent row, parent column)
+    p = np.zeros(
+        (h, w, 2),
+        dtype = np.dtype(int)
+    )
     for i in range(h):
         for j in range(w):
-            p[i, j, :] = [i, j]
-            if not data[i, j]: continue
-            k = []
-            if i >= 1 and data[i - 1, j]: k.append((i - 1, j))
-            if j >= 1 and data[i, j - 1]: k.append((i, j - 1))
-            if len(k) == 0: continue
-            for l, (ii, jj) in enumerate(k):
-                iii, jjj = p[ii, jj, :]
-                while (iii, jjj) != (ii, jj):
+            p[i, j, :] = [i, j]                                # init parent as self
+            if not data[i, j]: continue                        # if not filled here, do nothing
+            k = []                                             # potential parents
+            if i >= 1 and data[i - 1, j]: k.append((i - 1, j)) # top parent    *
+            if j >= 1 and data[i, j - 1]: k.append((i, j - 1)) # left parent  *┘
+            if len(k) == 0: continue                           # if no potential parents, leave parent as self, we are done
+            for l, (ii, jj) in enumerate(k):                   # for each potential parent indices ii jj, at index l in k    ┌──→∘
+                iii, jjj = p[ii, jj, :]                        #     get their parents                                       │ ∘─┴─∘
+                while (iii, jjj) != (ii, jj):                  #     keep stepping up parents to a root                      ×─┴─∘
                     ii, jj = iii, jjj
                     iii, jjj = p[ii, jj, :]
-                k[l] = (iii, jjj)
-            p[i, j, :] = k[0]
-            if len(k) == 1: continue
-            ii, jj = k[1]
-            p[ii, jj, :] = k[0]
+                k[l] = (iii, jjj)                              #     parent of potential parent is now their root
+            p[i, j, :] = k[0]                                  # choose the root of the first parent (arbitrary) as our parent
+            if len(k) == 1: continue                           # if there was only the one potential parent, we're done
+            ii, jj = k[1]                                      # if there was a second potential parent
+            p[ii, jj, :] = k[0]                                #     merge the trees
 
-    p = np.multiply(p, [w, 1]).sum(axis = 2).ravel()
-    q = p[p]
+    p = np.multiply(p, [w, 1]).sum(axis = 2).ravel() # convert to 1d parent vector
+    q = p[p]                                         # find roots of each pixel
     while not np.array_equiv(p, q):
         p = q
         q = p[p]
 
-    s = data.ravel() * (p + 1)
-    c = np.sort(np.unique(s))
-    s = np.searchsorted(c, s)
+    s = data.ravel() * (p + 1)                       # zero out non-filled nodes
+    c = np.sort(np.unique(s))                        # roots
+    s = np.searchsorted(c, s)                        # ids
 
-    min = np.zeros(len(c) - 1)
-    max = np.zeros(len(c) - 1)
-    for e in range(1, len(c)):
+    min = np.zeros(len(c) - 1)                       #  leftmost pixel indices
+    max = np.zeros(len(c) - 1)                       # rightmost pixel indices
+    for e in range(1, len(c)):                       # fill those bad boys in
         i = np.argwhere(s == e).ravel() % w
         min[e - 1] = i.min()
         max[e - 1] = i.max()
 
+    # adjacency matrix where x → y if min(x) ≥ min(y) and (max(x) ≤ max(y) or min(x) ≤ min(y) + (max(y) - min(y))/2)
+    # │# #        (  # #│       # #  )
+    # │ #         (   # │        #   )
+    # │#          (  #  │       #│   )
+    # │      and  (     │  or    │   )
+    # ││# #       ( # #││       │# # )
+    # ││ #        (  # ││       │ #  )
+    # ││# #       ( # #││       │# # )
+    # x is either contained in the bounds of y, or, if it goes far right of y, sunk far enough into the left of y
     b = np.logical_and(
         np.greater_equal.outer(min, min),
         np.logical_or(
@@ -51,15 +68,19 @@ def split(image):
             np.less_equal.outer(min, min + 0.5 * (max - min)),
         ),
     )
-    b = np.logical_or(b, b.T)
+    b = np.logical_or(b, b.T) # make undirected, now x ←→ y if at least one one overlaps the other
+    # find transitive closure
     bb = b.dot(b)
     while not np.array_equiv(b, bb):
         b = bb
         bb = b.dot(b)
 
+    # group together in s
     for e in range(1, len(c)):
         s[np.argwhere(s == e).ravel()] = np.nonzero(b[e - 1, :])[0][0] + 1
 
+    # as above, find the bounds of each (group of) glyphs
+    # this time put them into separate arrays
     # FIXME: it's inefficient to just do this all over, try save some results idk
     c = np.sort(np.unique(s))
     s = np.searchsorted(c, s)
